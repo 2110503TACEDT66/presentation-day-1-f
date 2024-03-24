1451fd467dec3d3de847f0d26a4c1f68de73acbd
Tin <tinnasmartlol@gmail.com>
2024-03-24 18:25:25 +0700

    add name

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,17 +46,24 @@ exports.login = async (req,res,next)=>{
 }
 }
 
-const sendTokenResponse = (user,statusCode,res)=>{
+const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
     const options = {
-        expires : new Date(Date.now()+ process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
-        httpOnly:true
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
     };
 
-    if(process.env.NODE_ENV ==='production'){
-        options.secure =true;
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
     }
-    res.status(statusCode).cookie('token',token,options).json({success:true,token});
+
+    res.status(statusCode)
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            token,
+            name: user.name // Including user's name in the response
+        });
 }
 
 exports.getMe = async (req,res,next)=>{
